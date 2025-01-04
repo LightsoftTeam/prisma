ca69d3fd3f8eb87d1ec799f6c0b63c717878a87c
José <josemr.99@outlook.com>
2025-01-04 12:52:51 -0500

    enable cors for local dev

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { writeFile } from 'fs/promises';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
